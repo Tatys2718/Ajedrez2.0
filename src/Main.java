bdcd5a51da28e9e199aa96f3b0b7405421610d04
León Cantillo <ldcantillom@unimagdalena.edu.co>
2024-11-01 08:33:28 -0500

    Aplicación de todods los metodos validMove en las clases de pieza.

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File file = new File ("src\\Partidas\\pawn_movements_test.txt");
+        File file = new File ("src\\Partidas\\pruebaConFe.txt");
         ReaderPGN reader = new ReaderPGN(file);
         reader.Lecture();
 
--- src/Model/King.java
+++ src/Model/King.java
@@ -6,4 +6,20 @@ public class King extends Piece{
     public King(int positionRow, int positionColumn, boolean isWhite, ImageIcon image) {
         super(positionRow, positionColumn, isWhite, image);
     }
+
+    @Override
+    public boolean validMove(int destRow, int destColumn, Piece[][] pieces) {
+        // Obtener la fila y columna actuales
+        int currentRow = getPositionRow();
+        int currentColumn = getPositionColumn();
+
+        // Verificar que el movimiento es de una sola casilla en cualquier dirección
+        if (Math.abs(destRow - currentRow) <= 1 && Math.abs(destColumn - currentColumn) <= 1) {
+            // Verificar si la casilla de destino está ocupada por una pieza del mismo color
+            Piece targetPiece = pieces[destRow][destColumn];
+            return targetPiece == null || targetPiece.isRed() != this.isRed();
+        }
+        return false; // Movimiento no válido
+    }
+
 }
